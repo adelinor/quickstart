8cd24097d9df5dd4665be57eaa8aabfafa0c4ce1
adelinor <adelinor@gmail.com>
2017-01-02 08:42:48 +0000

    Improve end to end test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,23 +1,20 @@
 import { browser, element, by } from 'protractor';
-import { TestBed } from '@angular/core/testing';
-
-import { CustomComponent } from '../app/custom.component';
 
 describe('QuickStart E2E Tests', function () {
 
   let expectedMsg = 'Hello Angular';
+  let expectedMsgFromComponent = 'This is a simple component';
 
   beforeEach(function () {
-    // declare the test component
-    TestBed.configureTestingModule({
-      declarations: [ CustomComponent ]
-    });
-
     browser.get('');
   });
 
   it('should display: ' + expectedMsg, function () {
     expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
+  });
+
+  it('should display: ' + expectedMsgFromComponent, function () {
+    expect(element(by.css('custom')).getText()).toEqual(expectedMsgFromComponent);
   });
 
 });
